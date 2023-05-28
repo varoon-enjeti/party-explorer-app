73b09868cffac6cc8d59b5c79ef1db6da155f6c7
Varoon Enjeti <97761722+varoon-enjeti@users.noreply.github.com>
2023-05-28 16:48:28 -0500

    Initial Commit

---
+++ Party Explorer App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Party Explorer App
+//
+//  Created by Varoon Enjeti on 5/28/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Party Explorer App/Party_Explorer_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Party_Explorer_AppApp.swift
+//  Party Explorer App
+//
+//  Created by Varoon Enjeti on 5/28/23.
+//
+
+import SwiftUI
+
+@main
+struct Party_Explorer_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
